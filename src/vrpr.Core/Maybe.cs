522b656bf41165b50f22871af9089a8b4f8d9ab4
Vadzim Vysotski <vysotski.vadim@gmail.com>
2016-04-10 10:04:43 +0300

    added core project with results and maybe

---
+++ src/vrpr.Core/Maybe.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NullGuard;
+
+namespace vrpr.Core
+{
+    public struct Maybe<T>
+    {
+        private readonly T _value;
+
+        public T Value
+        {
+            get
+            {
+                if (HasNoValue)
+                {
+                    throw new NotSupportedException("Maybe has not value");
+                }
+
+                return _value;
+            }
+        }
+
+        public bool HasValue
+        {
+            get { return _value != null; }
+        }
+
+        public bool HasNoValue => !HasValue;
+
+        private Maybe([AllowNull] T value)
+        {
+            _value = value;
+        }
+
+        public static implicit operator Maybe<T>([AllowNull] T value)
+        {
+            return new Maybe<T>(value);
+        }
+    }
+}
---
+++ src/vrpr.Core/Result.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace vrpr.Core
+{
+    public class Result
+    {
+        protected Result(bool success, string error)
+        {
+            Success = success;
+            Error = error;
+        }
+
+        public bool Success { get; private set; }
+
+        public bool Failed => !Success;
+
+        public string Error { get; private set; }
+
+        public static Result Ok()
+        {
+            return new Result(true, string.Empty);
+        }
+
+        public static Result Fail(string error)
+        {
+            return new Result(false, error);
+        }
+
+        public static Result<T> Ok<T>(T value)
+        {
+            return Result<T>.Ok(value);
+        }
+
+        public static Result<T> Fail<T>(string error)
+        {
+            return Result<T>.Fail(error);
+        } 
+    }
+
+    public class Result<T> : Result
+    {
+        private T _value;
+
+        protected Result(T value) : base(true, string.Empty)
+        {
+            Value = value;
+        }
+
+        protected Result(string error) : base(false, error)
+        {
+        } 
+
+        public T Value
+        {
+            get
+            {
+                if (Failed)
+                {
+                    throw new NotSupportedException("Can't get value from Failed Result");
+                }
+                return _value;
+            }
+            protected set { _value = value; }
+        }
+
+        public static Result<T> Ok(T value)
+        {
+            return new Result<T>(value);
+        }
+
+        public static Result<T> Fail(string message)
+        {
+            return new Result<T>(message);
+        } 
+    }
+}
