e6baf25ce64899d3e1e355fe4469d8c5ce02f225
Vadzim Vysotski <vysotski.vadim@gmail.com>
2016-04-10 15:27:54 +0300

    added ticeract processor

---
+++ src/vrpr.DesktopCore/Processors/TeseractOcrProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Emgu.CV;
+using Emgu.CV.Util;
+using Tesseract;
+using vrpr.Core.Infrastructure;
+
+namespace vrpr.DesktopCore.Processors
+{
+    public class TeseractOcrProcessor : IProcessor<Mat, string>
+    {
+        public Result<string> Process(Mat input)
+        {
+            try
+            {
+                var buff = new VectorOfByte();
+                CvInvoke.Imencode(".tiff", input, buff);
+                using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadTiffFromMemory(buff.ToArray()))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            var text = page.GetText();
+                            return Result.Ok(text);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return Result.Fail<string>(e.Message);
+            }
+        }
+    }
+}
