f33bd921864014f64191d5e23d17e701841419dd
Vadzim Vysotski <vysotski.vadim@gmail.com>
2016-04-15 19:03:20 +0300

    downgrade unity because of nancy

---
+++ src/vrpr.WebUi/Bootstapper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Bootstrappers.Unity;
+using vrpr.Core;
+using vrpr.DesktopCore;
+using vrpr.DesktopCore.DebugLog;
+using vrpr.WebUi.Logger;
+
+namespace vrpr.WebUi
+{
+    public class Bootstrapper : UnityNancyBootstrapper
+    {
+        protected override void ApplicationStartup(IUnityContainer container, IPipelines pipelines)
+        {
+            // No registrations should be performed in here, however you may
+            // resolve things that are needed during application startup.
+        }
+
+        protected override void ConfigureApplicationContainer(IUnityContainer existingContainer)
+        {
+            // Perform registation that should have an application lifetime
+            existingContainer.RegisterInstance<IUnityContainer>(existingContainer);
+            existingContainer.RegisterType<IProcessorFactory, ProcessorFactory>();
+            existingContainer.RegisterType<IVehicleRegistrationPlateRecognizer, VehicleRegistrationPlateRecognizer>();
+        }
+
+        protected override void ConfigureRequestContainer(IUnityContainer container, NancyContext context)
+        {
+            // Perform registrations that should have a request lifetime
+#pragma warning disable 618
+            container.RegisterType<IDebugLogBuilder, WebDebugLogBuilder>(new ContainerControlledLifetimeManager());
+#pragma warning restore 618
+            container.RegisterType<IDebugLogProvier, WebDebugLogBuilder>(new ContainerControlledLifetimeManager());
+            container.RegisterType<IDebugLogger, DebugLogger>(new ContainerControlledLifetimeManager());
+        }
+
+        protected override void RequestStartup(IUnityContainer container, IPipelines pipelines, NancyContext context)
+        {
+            // No registrations should be performed in here, however you may
+            // resolve things that are needed during request startup.
+        }
+    }
+}
---
+++ src/vrpr.WebUi/DevelperModule.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Nancy;
+using vrpr.DesktopCore;
+using vrpr.WebUi.Logger;
+
+namespace vrpr.WebUi
+{
+    public class DevelperModule : NancyModule
+    {
+        public DevelperModule(IVehicleRegistrationPlateRecognizer recognizer, IDebugLogProvier logProvider)
+        {
+            Get["/"] = p => View["selectFilesForm.html"];
+            Post["/"] = p =>
+            {
+                foreach (var file in Request.Files)
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        file.Value.CopyTo(ms);
+                        var imageData = ms.ToArray();
+                        recognizer.Process(imageData);
+                    }
+                }
+
+                var logs = logProvider.GetLog();
+                return View["log.html", new { Logs = logs }];
+            };
+        }
+    }
+}
