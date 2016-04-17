ca0ed64ad521643c711269accfc8312753593997
Vadzim Vysotski <vysotski.vadim@gmail.com>
2016-04-17 12:44:25 +0300

    add part-implemented fin terrers processor

---
+++ src/vrpr.DesktopCore/Processors/FindLettersProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using vrpr.Core.Infrastructure;
+using vrpr.DesktopCore.DebugLog;
+
+namespace vrpr.DesktopCore.Processors
+{
+    public class FindLettersProcessor : IProcessor<Mat, IEnumerable<Mat>>
+    {
+        private readonly IDebugLogger _debugLogger;
+
+        public FindLettersProcessor(IDebugLogger debugLogger)
+        {
+            _debugLogger = debugLogger;
+        }
+
+        public Result<IEnumerable<Mat>> Process(Mat input)
+        {
+            var binarized = new Mat();
+            CvInvoke.AdaptiveThreshold(input, binarized, 255, AdaptiveThresholdType.MeanC, ThresholdType.Binary, 7, 5);
+            _debugLogger.Log(logBuilder => logBuilder.AddMessage("Binarized image").AddImage(binarized));
+
+            var afterCanny = new Mat();
+            CvInvoke.Canny(binarized, afterCanny, 1, 3);
+            _debugLogger.Log(logBuilder => logBuilder.AddMessage("Canny result").AddImage(afterCanny));
+
+            return Result.Ok(new [] { input }.AsEnumerable());
+        }
+    }
+}
