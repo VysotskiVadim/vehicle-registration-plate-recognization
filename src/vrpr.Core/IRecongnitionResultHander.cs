527c5aacd72b8056d4b6d29b625ff58839bb358c
Vadzim Vysotski <Vadzim_Vysotski@epam.com>
2016-05-23 16:07:30 +0300

    add recognition result handerl

---
+++ src/vrpr.Core/IRecongnitionResultHander.cs
@@ -0,0 +1,10 @@
+﻿using Emgu.CV;
+
+namespace vrpr.Core
+{
+    public interface IRecongnitionResultHander
+    {
+        void HandleSuccess(string result, Mat image);
+        void HandleFaulure(Mat image);
+    }
+}
